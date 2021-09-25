50c9f8d9336252b2ea164d2d7310037469eae38e
elzup <guild0105@gmail.com>
2021-09-25 21:32:47 +0900

    fix scripts tsconfig WIP

--- scripts/dbload.ts
+++ scripts/dbload.ts
@@ -1,19 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-var-requires */
 import admin from 'firebase-admin'
 
 const serviceAccountProd = require('')
 const serviceAccountDev = require('./secrets/waiwaiwiki-staging-firebase-adminsdk-fmgdt-b284e578b6.json')
 
-const envfile = process.env.ENV_FILE || '.env.development'
-
 const serviceAccount =
-  envfile === '.env.development' ? serviceAccountDev : serviceAccountProd
+  process.env.ENV === 'prod' ? serviceAccountDev : serviceAccountProd
 
 // const app = firebase.initializeApp(config)
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-})
+const credential = admin.credential.cert(serviceAccount)
+admin.initializeApp({ credential })
 export const db = admin.database()
 export { admin }
--- scripts/seeds.ts
+++ scripts/seeds.ts
@@ -1,10 +1,11 @@
-import { admin, db } from './dbload'
+import * as dbload from './dbload'
 import { getUserOptional } from '../src/service/firebase'
 
 async function main() {
-  const devices = await db.ref('device').once('value')
+  const devices = await dbload.db.ref('device').once('value')
   getUserOptional()
 }
+
 main().then(() => {
-  admin.apps[0]?.delete()
+  dbload.admin.apps[0]?.delete()
 })
