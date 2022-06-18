ca7b8ed04637e7f18145f5fe5fcf0234259e5ec2
elzup <guild0105@gmail.com>
2022-06-18 22:03:55 +0900

    fix border

--- src/components/KoyomiPage/KoyomiBoard.tsx
+++ src/components/KoyomiPage/KoyomiBoard.tsx
@@ -1,4 +1,4 @@
-import { keyBy, schedulingBy } from '@elzup/kit'
+import { groupByFunc, keyBy, schedulingBy } from '@elzup/kit'
 import { Typography } from '@mui/material'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
@@ -12,12 +12,20 @@ import {
 } from '../../utils/koyomi'
 import { useKoyomiDraw } from './useKoyomiDraw'
 
+type GraphProps = {
+  rows: number
+  cols: number
+}
+
 type Props = {
   koyomis: Koyomi[]
 }
 
+const CH = 42
+const CW = 42
+
 function KoyomiBoard({ koyomis }: Props) {
-  const { blocks, measures } = useMemo(() => {
+  const { blocks, measures, graph } = useMemo(() => {
     const blocks: BlockLine[] = koyomis.map((koyomi) => ({
       koyomi,
       lines: schedulingBy(mapId(koyomi.times), ({ id, ...v }) => ({
@@ -28,42 +36,128 @@ function KoyomiBoard({ koyomis }: Props) {
     }))
     const range = getRangeKoyomi(koyomis)
 
-    return { blocks, measures: makeMeasure(range.bgn, range.end) }
+    const measures = makeMeasure(range.bgn, range.end)
+    const graph: GraphProps = {
+      cols: measures.length,
+      rows: blocks.map((v) => v.lines.length).reduce((a, b) => a + b),
+    }
+
+    return { blocks, measures, graph }
   }, [koyomis])
   const png = useKoyomiDraw(blocks, measures)
 
   return (
     <Style>
-      {png !== null && <img src={png} />}
-      {blocks.map(({ koyomi, lines }) => (
-        <div key={koyomi.id}>
-          <Typography>{koyomi.title}</Typography>
-
-          <div className="tl">
-            {lines.map((cells, ci) => (
-              <div key={ci} className="line">
-                {measures
-                  .map((pos) => ({ pos, cell: cells[pos.id] }))
-                  .map(({ pos, cell }) => (
-                    <div
-                      key={pos.id}
-                      className="cell"
-                      data-y={pos.ym.y}
-                      data-m={pos.ym.m}
-                    >
-                      {cell?.label}
+      <div className="draw">
+        <div className="draw-part head">
+          {Object.entries(groupByFunc(measures, (v) => String(v.ym.y))).map(
+            ([y, v]) => (
+              <div key={y} className="y">
+                <div className="y-head">{y}</div>
+                <div className="m">
+                  {v.map((m) => (
+                    <div key={m.id} className="m-cell">
+                      <div className="m-head">{m.ym.m}</div>
                     </div>
                   ))}
+                </div>
               </div>
-            ))}
-          </div>
+            )
+          )}
         </div>
-      ))}
+        <div className="draw-part body">
+          {Object.entries(groupByFunc(measures, (v) => String(v.ym.y))).map(
+            ([y, v]) => (
+              <div key={y} className="y">
+                <div className="m">
+                  {v.map((m) => (
+                    <div key={m.id} className="m-cell">
+                      <div
+                        className="body"
+                        style={{ height: `${CH * graph.rows}px` }}
+                      ></div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )
+          )}
+          {blocks.map(({ koyomi, lines }) => (
+            <div key={koyomi.id}>
+              <Typography>{koyomi.title}</Typography>
+
+              <div className="tl">
+                {lines.map((cells, ci) => (
+                  <div key={ci} className="line">
+                    {measures
+                      .map((pos) => ({ pos, cell: cells[pos.id] }))
+                      .map(({ pos, cell }) => (
+                        <div
+                          key={pos.id}
+                          className="cell"
+                          data-y={pos.ym.y}
+                          data-m={pos.ym.m}
+                        >
+                          {cell?.label}
+                        </div>
+                      ))}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>{png !== null && <img src={png} />}</div>
     </Style>
   )
 }
+
 const Style = styled.div`
+  .draw {
+  }
+
+  .draw-part {
+    display: grid;
+    grid-auto-flow: column;
+    .y {
+      border: solid 2px #aaa;
+      &:not(:first-child) {
+        border-left-width: 0;
+      }
+      .y-head {
+        padding-left: 4px;
+      }
+    }
+    .m {
+      display: grid;
+      grid-auto-flow: column;
+      text-align: center;
+      .m-cell {
+        width: ${CW}px;
+        border: solid 1px #aaa;
+        border-bottom-width: 0;
+        border-left-width: 0;
+        &:last-child {
+          border-right-width: 0;
+        }
+      }
+    }
+    &.head {
+      .y {
+        border-bottom-width: 0;
+      }
+    }
+    &.body {
+    }
+  }
+
+  .koyomi {
+    position: absolute;
+    display: none;
+  }
   .tl {
+    font-family: 'Noto Sans JP', sans-serif;
     .line {
       display: flex;
     }
