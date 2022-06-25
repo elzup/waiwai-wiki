8e633b829a45280d901f873b7331381c4fff95bd
elzup <guild0105@gmail.com>
2022-06-25 21:22:04 +0900

    add koyomi form

---
+++ src/components/KoyomiPage/KoyomiEditor/KoyomiForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Koyomi } from '../../../types'
+
+type Props = {
+  koyomi: Koyomi
+}
+function KoyomiForm(props: Props) {
+  return <div></div>
+}
+export default KoyomiForm
---
+++ src/components/KoyomiPage/KoyomiEditor/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Koyomi } from '../../../types'
+
+type Props = {
+  koyomi: Koyomi
+}
